ad1735269714e31b7bffd02ba626a95083ea4997
1Funso <>
2023-08-03 18:09:12 +0000

    Implemented user data input

--- run.py
+++ run.py
@@ -10,8 +10,7 @@ the data by gender, age, or weight category.
 
 import gspread
 from google.oauth2.service_account import Credentials
-import pandas as pd
-import numpy as np
+
 
 # Google Sheets authentication
 SCOPE = [
@@ -26,8 +25,27 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Obesity')
 
 # Open 'Obesity' worksheet
-Obesity = SHEET.worksheet('Obesity')
+obesity = SHEET.worksheet('Obesity')
 
-data = Obesity.get_all_values()
+def get_user_data():
+    """
+    Get user data input from the terminal. The user is prompted to enter
+    their age, gender, height, and weight, separated by commas. The function
+    continues to prompt the user until valid data is entered.
+    """
+    while True:
+        print("Please enter your age, gender, height, and weight.")
+        print("Data should be four values, separated by commas.")
+        print("Example: 25, Male, 180, 80\n")
 
-print(data)
+        data_str = input("Enter your data here: ")
+
+        user_data = data_str.split(",")
+
+        # Validate user data
+        if validate_input(user_data):
+            print("Data is valid!")
+            break
+
+    return user_data
+    
